798b20c80ab148b15e5752cdfe8433c6629a041d
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-13 00:31:09 -0300

    Atualizando __init__.py para o compilador encontrar ft2build.h

--- recipes/harfbuzz/__init__.py
+++ recipes/harfbuzz/__init__.py
@@ -6,8 +6,17 @@ class HarfbuzzPatchedRecipe(Recipe):
 
     def get_recipe_env(self, arch=None, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
-        env["CFLAGS"] += " -Wno-error=cast-function-type-strict"
-        env["CXXFLAGS"] += " -Wno-error=cast-function-type-strict"
+
+        # Diretório onde o freetype foi compilado
+        freetype_build = self.get_recipe('freetype', self.ctx).get_build_dir(arch.arch)
+        freetype_include = f"{freetype_build}/include"
+
+        # Inclui o caminho do freetype no include path e corrige warnings
+        env["CFLAGS"] += f" -I{freetype_include} -Wno-error=cast-function-type-strict"
+        env["CXXFLAGS"] += f" -I{freetype_include} -Wno-error=cast-function-type-strict"
+        env["LDFLAGS"] += f" -L{freetype_build}/objs/.libs"
+        env["PKG_CONFIG_PATH"] = f"{freetype_build}/objs"
+
         return env
 
 recipe = HarfbuzzPatchedRecipe()
