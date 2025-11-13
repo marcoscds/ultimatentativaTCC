6ea7795be79c3349409ac7202493b60bb08aed24
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-13 01:48:31 -0300

    Ignorandoerros __init__.py

--- recipes/harfbuzz/__init__.py
+++ recipes/harfbuzz/__init__.py
@@ -6,19 +6,26 @@ class HarfbuzzPatchedRecipe(Recipe):
 
     def get_recipe_env(self, arch=None, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
-        env["CFLAGS"] += " -Wno-error -Wno-unused-but-set-variable -Wno-error=cast-function-type-strict"
-        env["CXXFLAGS"] += " -Wno-error -Wno-unused-but-set-variable -Wno-error=cast-function-type-strict"
-        
+
+        # Ignora warnings que estão travando o build
+        flags = "-Wno-error -Wno-error=unused-but-set-variable -Wno-error=cast-function-type-strict"
+        env["CFLAGS"] += f" {flags}"
+        env["CXXFLAGS"] += f" {flags}"
+
         # Diretório onde o freetype foi compilado
         freetype_build = self.get_recipe('freetype', self.ctx).get_build_dir(arch.arch)
         freetype_include = f"{freetype_build}/include"
         freetype_include2 = f"{freetype_build}/include/freetype2"
 
         # Inclui ambos os caminhos do freetype
-        env["CFLAGS"] += f" -I{freetype_include} -I{freetype_include2} -Wno-error=cast-function-type-strict"
-        env["CXXFLAGS"] += f" -I{freetype_include} -I{freetype_include2} -Wno-error=cast-function-type-strict"
+        env["CFLAGS"] += f" -I{freetype_include} -I{freetype_include2}"
+        env["CXXFLAGS"] += f" -I{freetype_include} -I{freetype_include2}"
+
+        # Link com FreeType
         env["LDFLAGS"] += f" -L{freetype_build}/objs/.libs"
         env["PKG_CONFIG_PATH"] = f"{freetype_build}/objs"
+
+        return env
 
         return env
 
