fc49cf6f5d37266613567752cc5004eb80b73ed2
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-13 02:13:40 -0300

    Ignorando avisos __init__.py

--- recipes/harfbuzz/__init__.py
+++ recipes/harfbuzz/__init__.py
@@ -1,31 +1,32 @@
-from pythonforandroid.recipe import Recipe
+from pythonforandroid.recipe import AutotoolsRecipe
 
-class HarfbuzzPatchedRecipe(Recipe):
+class HarfbuzzPatchedRecipe(AutotoolsRecipe):
     version = "5.3.1"
     url = f"https://github.com/harfbuzz/harfbuzz/archive/refs/tags/{version}.tar.gz"
 
     def get_recipe_env(self, arch=None, **kwargs):
         env = super().get_recipe_env(arch, **kwargs)
 
-        # Ignora warnings que estão travando o build
+        # Remove qualquer -Werror herdado
+        env["CFLAGS"] = env.get("CFLAGS", "").replace("-Werror", "")
+        env["CXXFLAGS"] = env.get("CXXFLAGS", "").replace("-Werror", "")
+
+        # Ignora warnings específicos
         flags = "-Wno-error -Wno-error=unused-but-set-variable -Wno-error=cast-function-type-strict"
         env["CFLAGS"] += f" {flags}"
         env["CXXFLAGS"] += f" {flags}"
 
-        # Diretório onde o freetype foi compilado
+        # Caminhos do freetype
         freetype_build = self.get_recipe('freetype', self.ctx).get_build_dir(arch.arch)
         freetype_include = f"{freetype_build}/include"
         freetype_include2 = f"{freetype_build}/include/freetype2"
 
-        # Inclui ambos os caminhos do freetype
         env["CFLAGS"] += f" -I{freetype_include} -I{freetype_include2}"
         env["CXXFLAGS"] += f" -I{freetype_include} -I{freetype_include2}"
 
-        # Link com FreeType
+        # Linkagem com freetype
         env["LDFLAGS"] += f" -L{freetype_build}/objs/.libs"
         env["PKG_CONFIG_PATH"] = f"{freetype_build}/objs"
-
-        return env
 
         return env
 
