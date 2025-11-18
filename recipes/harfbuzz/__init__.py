78fae874c6393315208b20edd472a626f35a5436
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-18 14:55:19 -0300

    Update HARFBUZZ

--- recipes/harfbuzz/__init__.py
+++ recipes/harfbuzz/__init__.py
@@ -1,48 +1,16 @@
-import os
-from pythonforandroid.recipes.harfbuzz import HarfbuzzRecipe
-from pythonforandroid.util import current_directory, shprint
-import sh
+from pythonforandroid.recipe import AutoconfRecipe
 
-# Esta receita herda da receita Harfbuzz padrão do p4a,
-# para garantir que todos os passos de compilação (configure, make) sejam executados.
+class HarfbuzzFixedRecipe(AutoconfRecipe):
+    version = "8.0.1"
+    url = "https://github.com/harfbuzz/harfbuzz/releases/download/{version}/harfbuzz-{version}.tar.xz"
 
-class HarfbuzzFixRecipe(HarfbuzzRecipe):
-    """
-    Receita customizada para Harfbuzz que injeta flags de compilação
-    para desativar o erro -Werror (treat warnings as errors), resolvendo
-    falhas no NDK devido a variáveis não utilizadas (unused-but-set-variable).
-    """
-    # Sobrescreve o nome da receita, forçando o p4a a usá-la
-    name = 'harfbuzz'
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
 
-    def build_arch(self, arch):
-        # Pega as variáveis de ambiente base da receita padrão
-        env = self.get_recipe_env(arch)
+        # DESATIVA O WERROR QUE MATA O SEU BUILD
+        env["CFLAGS"] = env.get("CFLAGS", "") + " -Wno-error"
+        env["CXXFLAGS"] = env.get("CXXFLAGS", "") + " -Wno-error -Wno-unused-but-set-variable"
 
-        # A sua lista completa de flags para mitigar erros de NDK
-        no_error_flags = (
-            " -Wno-error"
-            " -Wno-error=unused-but-set-variable"
-            " -Wno-error=deprecated-declarations"
-            " -Wno-error=cast-function-type-strict"
-        )
+        return env
 
-        # --- CORREÇÃO CRÍTICA: INJETAR FLAGS DE COMPILAÇÃO ---
-        # Adiciona as flags de mitigação às variáveis CFLAGS e CXXFLAGS
-        env['CFLAGS'] = env.get('CFLAGS', '') + no_error_flags
-        env['CXXFLAGS'] = env.get('CXXFLAGS', '') + no_error_flags
-        # ----------------------------------------------------
-
-        with current_directory(self.get_build_dir(arch.arch)):
-            # Chama a compilação (make) com o ambiente modificado
-            shprint(sh.make, '-j', str(self.cpu_count), _env=env)
-
-            # Instalação padrão (herdada)
-            shprint(sh.make, 'install', _env=env)
-
-            # Limpeza e instalação final (herdada)
-            self.install_headers(arch)
-
-
-# O p4a espera que a variável de entrada tenha o mesmo nome da receita (harfbuzz)
-recipe = HarfbuzzFixRecipe()
+recipe = HarfbuzzFixedRecipe()
